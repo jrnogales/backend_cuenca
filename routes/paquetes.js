c778fdd21b9d3bf22774aba7c37f0e4295acc07a
jrnogales64 <jordynogalesg@gmail.com>
2025-10-20 10:34:16 -0500

    UI + estilos, controllers y SOAP listos

--- routes/paquetes.js
+++ routes/paquetes.js
@@ -1,2 +1,8 @@
-import express from 'express'; import { home, detalle } from '../controllers/paqueteController.js';
-const router = express.Router(); router.get('/',home); router.get('/paquetes/:codigo',detalle); export default router;
+import express from 'express';
+import { home, detalle } from '../controllers/paqueteController.js';
+const router = express.Router();
+
+router.get('/', home);
+router.get('/paquetes/:codigo', detalle);
+
+export default router;
